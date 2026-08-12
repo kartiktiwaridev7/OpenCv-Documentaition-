e7193567f96d2cd818e6535d6a72d85f53e6b8e7
Kartik Tiwari <kartiktiwaridev7@gmail.com>
2026-08-12 22:45:02 +0530

    Add files via upload

---
+++ Open CV/main.py
@@ -0,0 +1,12 @@
+import cv2
+
+image  = cv2.imread("ChatGPT Image Aug 5, 2026, 06_56_32 PM.png")
+
+if image is not None: 
+    cv2.imshow("Image showing" , image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+else:
+    print(" Image loded succesfully")
+
